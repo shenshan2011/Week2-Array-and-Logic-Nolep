3f4898751f6a1048c3fcf2ce9d16c5362c80c291
shenshan2011 <dana712.sesatism@gmail.com>
2025-03-09 19:58:10 +0700

    tugas1_part1_week2

---
+++ quiz/part1/tugas1.js
@@ -0,0 +1,70 @@
+/*
+Buatlah sebuah fungsi dengan nama dataHandling dengan sebuah parameter untuk menerima argumen. Argumen yang akan diterima adalah sebuah array yang berisi beberapa array sejumlah n.
+Tugas kamu adalah mengimplementasikan fungsi dataHandling agar dapat menampilkan data-data pada dari argumen seperti di bawah ini:
+*/
+let int = [
+    ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+    ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+    ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+    ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+]
+
+{
+    {console.log("Nomor ID:  " + int[0][0])
+        console.log("Nama Lengkap:  " + int[0][1])
+        console.log("TTL:  " + int[0][2] + " " +int[0][3])
+        console.log("Hobi:  " + int[0][4])
+        console.log(" ")
+    }
+}
+
+{
+    {console.log("Nomor ID:  " + int[1][0])
+        console.log("Nama Lengkap:  " + int[1][1])
+        console.log("TTL:  " + int[1][2] + " " +int[1][3])
+        console.log("Hobi:  " + int[1][4])
+        console.log(" ")
+    }
+}
+
+{
+    {console.log("Nomor ID:  " + int[2][0])
+        console.log("Nama Lengkap:  " + int[2][1])
+        console.log("TTL:  " + int[2][2] + " " +int[2][3])
+        console.log("Hobi:  " + int[2][4])
+        console.log(" ")
+    }
+}
+
+{
+    {console.log("Nomor ID:  " + int[3][0])
+        console.log("Nama Lengkap:  " + int[3][1])
+        console.log("TTL:  " + int[3][2] + " " +int[3][3])
+        console.log("Hobi:  " + int[3][4])
+        console.log(" ")
+    }
+}
+
+/*
+output yang diharapkan
+
+Nomor ID:  0001
+Nama Lengkap:  Roman Alamsyah
+TTL:  Bandar Lampung 21/05/1989
+Hobi:  Membaca
+
+Nomor ID:  0002
+Nama Lengkap:  Dika Sembiring
+TTL:  Medan 10/10/1992
+Hobi:  Bermain Gitar
+
+Nomor ID:  0003
+Nama Lengkap:  Winona
+TTL:  Ambon 25/12/1965
+Hobi:  Memasak
+
+Nomor ID:  0004
+Nama Lengkap:  Bintang Senjaya
+TTL:  Martapura 6/4/1970
+Hobi:  Berkebun
+*/
